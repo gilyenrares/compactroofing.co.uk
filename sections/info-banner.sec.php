5b2e547092deb1e7e162405616148791037f8f7c
gilyenrares <gilyenrares@users.noreply.github.com>
2020-07-09 18:43:48 +0100

    Update info-banner.sec.php

--- sections/info-banner.sec.php
+++ sections/info-banner.sec.php
@@ -4,28 +4,28 @@
             <div class="col-sm-4">
                 <div class="row my-4 justify-content-between">
                     <div class="col">
-                        <a href="https://www.facebook.com/TopRoofersltd-699861770475025" target="_blank"><img src="images/f-icon.png" style="max-height: 100%; max-width: 100px;"></a><br>
+                        <a href="#" target="_blank"><img src="images/f-icon.png" style="max-height: 100%; max-width: 100px;"></a><br>
                         <!-- <a href="#" target="_blank" class="text-decoration-none">toproofersltd</a> -->
                     </div>
                     <div class="col">
-                        <a href="https://www.instagram.com/toproofersltd" target="_blank"><img src="images/i-icon.png" style="max-height: 100%; max-width: 100px;"></a><br>
+                        <a href="#" target="_blank"><img src="images/i-icon.png" style="max-height: 100%; max-width: 100px;"></a><br>
                         <!-- <a href="#" target="_blank" class="text-decoration-none">toproofersltd</a> -->
                     </div>
                 </div>
             </div>
         	<div class="col-sm-4">
           		<div class="row justify-content-sm-center">
-                     <img src="images/logo-min.png" style="max-height: 150px; max-width: 200px;">      
+                   <!--   <img src="images/logo-min.png" style="max-height: 150px; max-width: 200px;">  -->     
                 </div>
         	</div>
             <div class="col-sm-4">
                 <div class="row my-4 justify-content-between">
                     <div class="col">
-                        <a href="mailto:info@toproofersltd.com"><img src="images/e-icon.png" style="max-height: 100%; max-width: 100px;"></a><br>
+                        <a href="mailto:#"><img src="images/e-icon.png" style="max-height: 100%; max-width: 100px;"></a><br>
                         <!-- <a href="mailto:info@toproofersltd.com"  class="text-decoration-none ml-4">Email Us</a> -->
                     </div>
                     <div class="col">
-                        <a href="tel:+4475 960 92240"><img src="images/p-icon.png" style="max-height: 100%; max-width: 100px;"></a><br>
+                        <a href="tel:+447380371224"><img src="images/p-icon.png" style="max-height: 100%; max-width: 100px;"></a><br>
                         <!-- <a href="tel:+4475 960 92240"  class="text-decoration-none ml-4">Call Us</a> -->
                     </div>
                 </div>
